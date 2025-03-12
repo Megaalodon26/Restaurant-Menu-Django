89cfc7e744ec4cec3b4b1f64d7058a28a5c960d2
Lynn, Megan <Megan_Lynn@comcast.com>
2025-03-12 15:32:04 -0400

    Added jinja if conditionals, cleaned up the code and formatting

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 MEAL_TYPE = (
     ("starters", "Starters"),
     ("salads", "Salads"),
-    ("main_dishes", "Main_Dishes"),
+    ("main_dishes", "Main Dishes"),
     ("desserts", "Desserts")
 )
 
